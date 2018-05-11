59b73360f4a4ab28f891f4dd63b6ada452bc826e
administrator <liquidnuker@users.noreply.github.com>
2018-05-10 19:18:28 -0700

    RkPager5 added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import RkPager1 from "./RkPager1.jsx";
 import RkPager2 from "./RkPager2.jsx";
 import RkPager3 from "./RkPager3.jsx";
 import RkPager4 from "./RkPager4.jsx";
+import RkPager5 from "./RkPager5.jsx";
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -19,7 +20,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <RkPager1  />
+        <RkPager1 />
         <br />
         <br />
         <RkPager2 />
@@ -29,6 +30,9 @@ export default class Home extends React.Component {
         <br />
         <br />
         <RkPager4 />
+        <br />
+        <br />
+        <RkPager5 />
       </div>
     );
   }
